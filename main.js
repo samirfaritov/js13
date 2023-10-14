02eee9df12c6164a66d64ea9a7925a1f1ef08ca9
samirfaritov <samirfaritov6@gmail.com>
2023-10-14 20:57:55 +0500

    цикл ichida цикл

---
+++ main.js
@@ -0,0 +1,118 @@
+// let a = [5, 9 , 5, 15 , 8787, 8787, 8, 9, 9, 9, ]
+// let b = [1, 6, 8, 9, 9, 8, 7, 45, 45, 45, 4, 7, 8, 6 ,32 , 5,]
+
+// let birxil = []
+
+// for(let i = 0; i < a.length; i++) {
+//     for(let k = 0; k < b.length; k++) {
+//         if(a[i] === b[k]) {
+//             birxil.push(a[i])
+//         }
+//     }
+// }
+
+// console.log(birxil);
+
+
+let names = ['Alisa', "Rano", "Anis", "Ilhom", "Jahongir", "Javohir", "Samir", "Jasur", "Kamola", "Dilnoz", "Dilafruz", "Dildora", "Lalitto", "lola", "Dilbar", "Diyora", "Shaxrizoda", "Sarvinoz", "Shaxina", "Sitora"]
+
+let users = [
+    {
+        name: 'Alisa',
+        money: 5000,
+        gender: 'female'
+    },
+    {
+        name: 'Rano',
+        money: 500,
+        gender: 'female'
+    },
+    {
+        name: 'Anis',
+        money: 3000,
+        gender: 'male'
+    },
+    {
+        name: 'Ilhom',
+        money: 2000,
+        gender: 'male'
+    },
+    {
+        name: 'Jahongir',
+        money: 900,
+        gender: 'male'
+    },
+    {
+        name: 'Samir',
+        money: 50,
+        gender: 'male'
+    },
+    {
+        name: 'Jasur',
+        money: 700,
+        gender: 'male'
+    },
+    {
+        name: 'Kamola',
+        money: 54800,
+        gender: 'female'
+    },
+    {
+        name: 'Dilnoz',
+        money: 540,
+        gender: 'female'
+    },
+    {
+        name: 'Dilafruz',
+        money: 560,
+        gender: 'female'
+    },
+    {
+        name: 'Dildora',
+        money: 90,
+        gender: 'female'
+    }
+]
+
+
+let genders = {
+    male: 0,
+    female: 0
+}
+
+let total = 0
+
+// for(let i = 0; i < users.length; i++) {
+//     for(let k = 0; k < users.length; k++) {
+//         if(users[i].gender === "male"[k]) {
+//             console.log(genders.male);
+//         }
+//     }
+// }
+
+// console.log(genders.male);
+
+// for(let i = 0; i < users.length; i++) {
+//     if (users[i].gender === 'female') {
+//         genders.female = ++genders.female
+//     } else {
+//         genders.male = ++genders.male
+
+//     }
+
+//     total += users[i].money
+// }
+
+// console.log(genders);
+// console.log(total);
+
+
+
+// for(let i = 0; i < users.length; i++) {
+//     console.log(names[i]);
+//     for(let k = 0; k < users.length; k++) {
+//         if(names[i] === users[k].name) {
+            
+//         }
+//     }
+// }
